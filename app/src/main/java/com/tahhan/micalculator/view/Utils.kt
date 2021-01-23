f499b25b6a56a648a4e543ad85915af13efe7f01
AmrMekhemar <amr.wf49@gmail.com>
2021-01-23 20:26:56 +0200

    added comments to Utils.kt

--- app/src/main/java/com/tahhan/micalculator/view/Utils.kt
+++ app/src/main/java/com/tahhan/micalculator/view/Utils.kt
@@ -24,6 +24,16 @@ private val primaryColor by lazy {
     ContextCompat.getColor(context, R.color.purple_700)
 }
 
+/**
+ * Makes buttons clickable
+ *
+ * @param button1
+ * @param button2
+ * @param button3
+ * @param button4
+ * @param enterNumberEditText
+ */
+
 fun View.makeButtonsClickable(
     button1: Button,
     button2: Button, button3: Button, button4: Button, enterNumberEditText: EditText
@@ -39,6 +49,14 @@ fun View.makeButtonsClickable(
     enterNumberEditText.text?.clear()
 }
 
+/**
+ * Make buttons non clickable depending on the operation
+ *
+ * @param operation
+ * @param button1
+ * @param button2
+ * @param button3
+ */
 fun View.makeButtonsNonClickable(
     operation: String,button1: Button,
     button2: Button, button3: Button
@@ -61,7 +79,7 @@ fun View.makeButtonsNonClickable(
 }
 
 private fun setAsNonClickable(button1: Button,
-                                     button2: Button,  button3: Button) {
+                              button2: Button,  button3: Button) {
     button1.isClickable = false
     button2.isClickable = false
     button3.isClickable = false
